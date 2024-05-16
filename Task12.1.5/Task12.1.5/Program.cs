0fecdfe6867f838dabac66f993d47549b79afe8c
Serg8181 <147308625+Serg8181@users.noreply.github.com>
2024-05-16 14:22:21 +0300

    Модуль 12. Задание 1.5

---
+++ Task12.1.5/Task12.1.5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*Для списка пользователей добавьте приветствие по имени.
+  Если пользователь не имеет премиум-подписки, то ему нужно показать рекламу.*/
+
+namespace Task12._1._5
+{
+    internal class Programm
+    {
+
+        public static void Main()
+        {
+            List<User> users = new List<User>()
+            {
+                new User("First","Дмитрий",true),
+                new User("Second","Вадим",false),
+                new User("Third","Виталий",true)
+            };
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Добрый день, {user.Name}!");
+                if (!user.IsPremium) ShowAds();
+            }
+            Console.ReadLine();
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+   
+}
---
+++ Task12.1.5/Task12.1.5/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12._1._5
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string log, string name,bool isPrem)
+        { 
+            Login = log;
+            Name = name;
+            IsPremium = isPrem;
+        }
+    }
+}
